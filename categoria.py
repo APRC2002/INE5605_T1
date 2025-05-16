60b6880171b665b22bedeb4fb899df027d6e8e1c
APRC2002 <andre.caceres@bridge.ufsc.br>
2025-05-16 18:59:16 -0300

    Delete unused files

--- categoria.py
+++
@@ -1,9 +0,0 @@
-
-from filme import Filme
-from voto import Voto
-
-class Categoria:
-    def __init__(self, nome: str):
-        self.__nome = nome
-        self.__filmes = []
-        self.__votos = []
