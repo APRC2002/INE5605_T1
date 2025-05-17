ed80fddb95bde63d4690445ddb2c805c3de5be5a
Caique Sales de Miranda Gomes <80981281+kiqsmg@users.noreply.github.com>
2025-05-17 18:50:39 -0300

    Update tela_diretor.py

---
+++ limite/tela_diretor.py
@@ -0,0 +1,40 @@
+class TelaDiretor():
+  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+  def tela_opcoes(self):
+    print("-------- DIRETOR ----------")
+    print("Escolha a opcao")
+    print("1 - Incluir Diretor")
+    print("2 - Alterar Diretor")
+    print("3 - Listar Diretor")
+    print("4 - Excluir Diretor")
+    print("0 - Retornar")
+
+    opcao = int(input("Escolha a opcao: "))
+    return opcao
+
+  #fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+  def pega_dados_diretor(self):
+    print("-------- DADOS DIRETOR ----------")
+    id = input("id: ")
+    nome = input("nome: ")
+    data_de_nascimento = input("data de nascimento: ")
+    nacionalidade = input("nacionalidade: ")
+    
+
+    return {"id": id, "nome": nome, "data_de_nascimento": data_de_nascimento, "nacionalidade": nacionalidade}
+
+  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+  def mostra_diretor(self, dados_diretor):
+    print("ID DO DIRETORR: ", dados_diretor["id"])
+    print("NOME DO DIRETOR: ", dados_diretor["nome"])
+    print("DATA DE NASCIMENTO DO DIRETOR: ", dados_diretor["data_de_nascimento"])
+    print("NACIONALIDADE DO DIRETOR: ", dados_diretor["nacionalidade"])
+    print("\n")
+
+  #fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+  def seleciona_diretor(self):
+    id = input("Id do diretor que deseja selecionar: ")
+    return id
+
+  def mostra_mensagem(self, msg):
+    print(msg)
