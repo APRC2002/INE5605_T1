8454b615562015c65ada3f05244fe628a3c7065e
Caique Sales de Miranda Gomes <80981281+kiqsmg@users.noreply.github.com>
2025-05-16 21:15:22 -0300

    Update controlador_ator.py

--- controle/controlador_ator.py
+++ controle/controlador_ator.py
@@ -5,42 +5,45 @@ class ControladorAtor():
 
   # Fazer lançamento e tratamento de exceções, ao invés de apenas mostrar mensagem na tela.
   def __init__(self, controlador_sistema):
-    self.__livros = []
+    self.__atores = []
     self.__controlador_sistema = controlador_sistema
-    self.__tela_livro = TelaLivro()
+    self.__tela_ator = TelaAtor()
 
-  def pega_livro_por_codigo(self, codigo: int):
-    for livro in self.__livros:
-      if(livro.codigo == codigo):
-        return livro
+  def pega_ator_por_id(self, id: int):
+    for ator in self.__atores:
+      if(ator.id == id):
+        return ator
     return None
 
-  def incluir_livro(self):
-    dados_livro = self.__tela_livro.pega_dados_livro()
-    l = self.pega_livro_por_codigo(dados_livro["codigo"])
+  def incluir_ator(self):
+    dados_ator = self.__tela_ator.pega_dados_ator()
+    l = self.pega_ator_por_id(dados_ator["id"])
     if l is None:
-      livro = Livro(dados_livro["titulo"], dados_livro["codigo"])
-      self.__livros.append(livro)
+      ator = Ator(dados_ator["nome"], dados_ator["id"], dados_ator["data_de_nascimento"], dados_ator["nacionalidade"], dados_ator["genero"])
+      self.__atores.append(ator)
     else:
-      self.__tela_livro.mostra_mensagem("ATENCAO: Livro já existente")
+      self.__tela_ator.mostra_mensagem("ATENCAO: Ator já existente")
 
-  def alterar_livro(self):
-    self.lista_livro()
-    codigo_livro = self.__tela_livro.seleciona_livro()
-    livro = self.pega_livro_por_codigo(codigo_livro)
+  def alterar_ator(self):
+    self.lista_ator()
+    id_ator = self.__tela_livro.seleciona_ator()
+    ator = self.pega_ator_por_id(id_ator)
 
-    if(livro is not None):
-      novos_dados_livro = self.__tela_livro.pega_dados_livro()
-      livro.titulo = novos_dados_livro["titulo"]
-      livro.codigo = novos_dados_livro["codigo"]
-      self.lista_livro()
+    if(ator is not None):
+      novos_dados_ator = self.__tela_ator.pega_dados_ator()
+      ator.nome = novos_dados_ator["nome"]
+      ator.id = novos_dados_ator["id"]
+      ator.data_de_nascimento = novos_dados_ator["data_de_nascimento"]
+      ator.nacionalidade = novos_dados_ator["nacionalidade"]
+      ator.genero = novos_dados_ator["genero"]
+      self.lista_ator()
     else:
-      self.__tela_livro.mostra_mensagem("ATENCAO: Livro não existente")
+      self.__tela_ator.mostra_mensagem("ATENCAO: Ator não existente")
 
   # Sugestão: se a lista estiver vazia, mostrar a mensagem de lista vazia
-  def lista_livro(self):
-    for livro in self.__livros:
-      self.__tela_livro.mostra_livro({"titulo": livro.titulo, "codigo": livro.codigo})
+  def lista_ator(self):
+    for ator in self.__atores:
+      self.__tela_ator.mostra_ator({"titulo": livro.titulo, "codigo": livro.codigo})
 
   def excluir_livro(self):
     self.lista_livro()
