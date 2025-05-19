5db36a75e7ca411424a8b574b61aa7fab86b2147
Caique Sales de Miranda Gomes <80981281+kiqsmg@users.noreply.github.com>
2025-05-19 19:40:18 -0300

    criar incluir_votacao

--- controle/controlador_votacao.py
+++ controle/controlador_votacao.py
@@ -1,29 +1,28 @@
-from limite.tela_ator import TelaAtor
-from entidade.ator import Ator
+from limite.tela_votacao import TelaVotacao
+from entidade.voto import Voto
 
-class ControladorAtor():
+
+class ControladorVotacao():
 
   # Fazer lançamento e tratamento de exceções, ao invés de apenas mostrar mensagem na tela.
   def __init__(self, controlador_sistema):
-    self.__atores = []
+    self.__votos = []
     self.__controlador_sistema = controlador_sistema
-    self.__tela_ator = TelaAtor()
+    self.__tela_votacao = TelaVotacao()
+    
 
-  def pega_ator_por_id(self, id: int):
-    for ator in self.__atores:
-      if(ator.id == id):
-        return ator
-    return None
+  def incluir_voto(self):
+    self.__controlador_sistema.controlador_membroAcademia.lista_membros()
+    self.__controlador_sistema.controlador_categorias.lista_categorias()
+    dados_voto = self.__tela_votacao.pega_dados_voto()
 
-  def incluir_ator(self):
-    dados_ator = self.__tela_ator.pega_dados_ator()
-    a = self.pega_ator_por_id(dados_ator["id"])
-    if a is None:
-      ator = Ator(dados_ator["id"], dados_ator["nome"], dados_ator["data_de_nascimento"], dados_ator["nacionalidade"], dados_ator["genero"])
-      self.__atores.append(ator)
-      return ator
+    membros_Academia = self.__controlador_sistema.controlador_membrosAcademia.pega_membro_por_id(dados_voto["id"])
+    categorias = self.__controlador_sistema.controlador_categorias.pega_categoria_por_nome(dados_voto["nome"])
+    if (membro_Academia is not None and categoria is not None):
+      voto = Voto(membro_academia, categoria)
+      self.__votos.append(voto)
     else:
-      self.__tela_ator.mostra_mensagem("ATENCAO: Ator já existente")
+      self.__tela_votacao.mostra_mensagem("ATENCAO: Voto já existente")
 
   def alterar_ator(self):
     self.lista_atores()
@@ -61,8 +60,8 @@ class ControladorAtor():
     self.__controlador_sistema.abre_tela()
 
   def abre_tela(self):
-    lista_opcoes = {1: self.alterar_ator, 2: self.lista_atores, 0: self.retornar}
+    lista_opcoes = {1: self.incluir_voto, 2: self.altera_voto, 3: self.lista_voto, 4: self.exclui_voto, 0: self.retornar}
 
     continua = True
     while continua:
-      lista_opcoes[self.__tela_ator.tela_opcoes()]()
+      lista_opcoes[self.__tela_votacao.tela_opcoes()]()
