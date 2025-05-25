d484b89d0cad5c67203fc2cb27eccfef77950264
APRC2002 <andreprcaceres@gmail.com>
2025-05-25 20:34:53 -0300

    Add remover_indicado() and excluir_categoria() methods to controlador_categoria.py

--- controle/controlador_categoria.py
+++ controle/controlador_categoria.py
@@ -63,26 +63,47 @@ class ControladorCategoria():
       self.__tela_categoria.mostra_mensagem("AVISO: Esta categoria já possui o número máximo de indicados")
 
   def remover_indicado(self):
-    pass
+    self.listar_categorias()
+    nome_categoria = self.__tela_categoria.pega_nome()
+    for c in self.__categorias:
+      if c.nome == nome_categoria:
+        categoria = c
+
+    self.__tela_categoria.mostra_mensagem(f"Os indicados da categoria {categoria.nome} são {categoria.indicados}")
+    indicado = self.__tela_categoria.pega_indicado()
+    for i in categoria.indicados:
+      if i.titulo == indicado:
+        categoria.indicados.remove(i)
 
   def listar_categorias(self):
     self.__tela_categoria.mostra_mensagem("----------------CATEGORIAS CADASTRADAS----------------")
-    for c in self.__categorias:
-      indicados = []
-      for indicado in c.indicados:
-        indicados.append(indicado.titulo)
-      self.__tela_categoria.mostra_categoria({"nome": c.nome, "indicados": indicados})
+    if len(self.__categorias) == 0:
+      self.__tela_categoria.mostra_mensagem("")
+      self.__tela_categoria.mostra_mensagem("AVISO: Não há categorias cadastradas")
+      self.__tela_categoria.mostra_mensagem("")
+    else:
+      for c in self.__categorias:
+        indicados = []
+        for indicado in c.indicados:
+          indicados.append(indicado.titulo)
+        self.__tela_categoria.mostra_categoria({"nome": c.nome, "indicados": indicados})
     self.__tela_categoria.mostra_mensagem("------------------------------------------------------")
     self.__tela_categoria.mostra_mensagem("")
 
   def excluir_categoria(self):
-    pass
+    self.listar_categorias()
+    categoria = self.__tela_categoria.pega_nome()
+    categoria = self.pega_categoria(categoria)
+    if categoria is not None:
+      self.__categorias.remove(categoria)
+    else:
+      self.__tela_categoria.mostra_mensagem("Categoria não cadastrada")
 
   def retornar(self):
     self.__controlador_sistema.abre_tela()
 
   def abre_tela(self):
-    lista_opcoes = {1: self.incluir_categoria, 2: self.alterar_nome_categoria, 3: self.adicionar_indicados, 4: self.remover_indicado, 5: self.listar_categorias, 7: self.excluir_categoria, 0: self.retornar}
+    lista_opcoes = {1: self.incluir_categoria, 2: self.alterar_nome_categoria, 3: self.adicionar_indicados, 4: self.remover_indicado, 5: self.listar_categorias, 6: self.excluir_categoria, 0: self.retornar}
 
     continua = True
     while continua:
