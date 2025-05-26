2c0f3b0f98fd2f972ecd41b3b2e9481eaacbdfd2
kiqsmg <kiqsmg@gmail.com>
2025-05-26 17:02:41 -0300

    Fix exception gender bug

--- exceptions/generoInvalidoException.py
+++ exceptions/generoInvalidoException.py
@@ -1,4 +1,4 @@
-class generoInvalidoException(Exception):
+class GeneroInvalidoException(Exception):
     def __init__(self, genero):
         self.mensagem = 'O genero {} é inválido. Gêneros válidos são "M" e "F".'
         super().__init__(self.mensagem.format(genero))
