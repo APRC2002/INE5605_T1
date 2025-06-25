f031a8e2b2db75226ca7cd0cc589f2e4e4ecb6c5
kiqsmg <kiqsmg@gmail.com>
2025-06-24 21:42:18 -0300

    Update sistema screen to support the PySimple User Interface

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,27 +1,55 @@
+import PySimpleGUI as sg
+
 class TelaSistema:
-    
-    def le_num_inteiro(self, mensagem=" ", ints_validos = None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
     
     def tela_opcoes(self):
-        print("-------- Premiação de filmes nacionais que não premia atores ---------")
-        print("Escolha sua opcao")
-        print("1 - Membros Academia")
-        print("2 - Atores")
-        print("3 - Diretores")
-        print("4 - Filmes")
-        print("5 - Categorias")
-        print("6 - Votação")
-        print("0 - Finalizar sistema")
-        opcao = self.le_num_inteiro("Escolha uma opção:", [0, 1, 2, 3, 4, 5, 6])
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['5']:
+            opcao = 5
+        if values['6']:
+            opcao = 6
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0'] or button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
         return opcao
+
+    def init_opcoes(self):
+        #sg.theme_previewer()
+        sg.theme('DarkTeal4')
+        layout = [
+            [sg.Text('-------- Premiação de filmes nacionais que não premia atores ---------', font=("Helvica", 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+            [sg.Radio('Membros Academia', "RD1", key='1')],
+            [sg.Radio('Atores', "RD1", key='2')],
+            [sg.Radio('Diretores', "RD1", key='3')],
+            [sg.Radio('Filmes', "RD1", key='4')],
+            [sg.Radio('Categorias', "RD1", key='5')],
+            [sg.Radio('Votação', "RD1", key='6')],
+            [sg.Radio('Finalizar sistema', "RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de Premiação').Layout(layout)
+
+    def mostra_mensagem(self, msg):
+        sg.popup("", msg)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
