b6f6b5fa60f40d1b2f5777e7c6c4675214f209dd
kiqsmg <kiqsmg@gmail.com>
2025-06-29 23:50:24 -0300

    Delete unecessary exceptions that are not beign used

--- exceptions/entradaInvalida.py
+++
@@ -1,4 +0,0 @@
-class entradaInvalida(Exception):
-    def __init__(self, entrada):
-        self.mensagem = 'A entrada "{}" não é válida.'
-        super().__init__(self.mensagem.format(entrada))
--- exceptions/idRepetidoException.py
+++
@@ -1,4 +0,0 @@
-class IdRepetidoException(Exception):
-    def __init__(self, id):
-        self.mensagem = 'O ID "{}" já existe.'
-        super().__init__(self.mensagem.format(id))
